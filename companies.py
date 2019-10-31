a2255f5ec2e7e7d2c50e1d9d5b4241ad8cf37d51
Ubuntu <ubuntu@ip-10-10-1-58.sa-east-1.compute.internal>
2019-10-31 16:40:12 -0300

    Carrega dados do site Defesa do Consumidor

---
+++ companies.py
@@ -0,0 +1,145 @@
+
+### Normaliza nome das empresas
+def parseName(name):
+    ### Limpeza
+    new_name = name.upper().replace(' (CONGLOMERADO)','')
+    new_name = new_name.replace('-','').replace(',','')
+    new_name = new_name.replace('S.A.','').replace('S A','').replace('S/A','').replace('S.A','')
+    new_name = new_name.replace('LTDA.','')
+    new_name = new_name.replace('CRÉDITO FINANCIAMENTO E INVESTIMENTO','').replace('CREDITO FINANCIAMENTO E INVESTIMENTO','').replace('CREDITO FINANCIAMENTO E INVESTIMENTOS','')
+    new_name = new_name.replace('SOCIEDADE DE','')
+    new_name = new_name.replace('FINANCIADORA','')
+    new_name = new_name.strip()
+    new_name = ' '.join(new_name.split())
+    new_name = new_name.replace('CRÉDITO FINANCIAMENTO E INVESTIMENTO','').replace('CREDITO FINANCIAMENTO E INVESTIMENTO','').replace('CREDITO FINANCIAMENTO E INVESTIMENTOS','').strip()
+    ### padronizacao
+    
+    if new_name.upper().startswith(('MERCADO LIBRE','MERCADOLI')):
+        new_name = 'MERCADO LIVRE'
+    elif new_name.upper().startswith('AMAZON'):
+        new_name = 'AMAZON SHOPPING'
+    elif new_name.upper().startswith('AMERICANAS'):
+        new_name = 'AMERICANAS'
+    elif new_name.upper().startswith('SUBMARINO'):
+        new_name = 'SUBMARINO'
+    elif new_name.upper().startswith('SHOPTIME'):
+        new_name = 'SHOPTIME'
+    elif new_name.upper().startswith(('MAGAZINE LUIZA','MAGAZINELUIZA')):
+        new_name = 'MAGAZINE LUIZA'
+    elif new_name.upper().startswith(('CASAS BAHIA','CASASBAHIA')):
+        new_name = 'CASAS BAHIA'
+    elif new_name.upper().startswith('PONTOFRIO'):
+        new_name = 'PONTO FRIO'
+    elif new_name.upper().startswith('EXTRA'):
+        new_name = 'EXTRA'
+    elif new_name.upper().startswith(('WALMEX','WALMART')):
+        new_name = 'WALMART'
+    elif new_name.upper().startswith('PICPAY'):
+        new_name = 'PICPAY'
+    elif new_name.upper().startswith(('MERCADO PAGO','MERCADOPAGO')):
+        new_name = 'MERCADO PAGO'
+    elif new_name.upper().startswith(('RECARGA CELULAR','RECARGAPAY')):
+        new_name = 'RECARGAPAY'
+    elif new_name.upper().startswith(('BANCO INTER','BANCOINTER')):
+        new_name = 'INTER'
+    elif new_name.upper().startswith('PAGSEGURO VENDAS'):
+        new_name = 'PAGSEGURO VENDAS'
+    elif new_name.upper().startswith('NEON'):
+        new_name = 'NEON'
+    elif new_name.upper().startswith(('BANCO NEXT','NEXT')):
+        new_name = 'NEXT'
+    elif new_name.upper().startswith('NUBANK'):
+        new_name = 'NUBANK'
+    elif new_name.upper().startswith(('BANCO ORIGINAL','ORIGINAL')):
+        new_name = 'ORIGINAL'
+    elif new_name.upper().startswith('STONE'):
+        new_name = 'STONE'
+    elif new_name.upper().startswith(('C6 BANK','C6BANK')):
+        new_name = 'C6 BANK'
+    elif new_name.upper().startswith('BS2 FLAMENGO'):
+        new_name = 'BS2 FLAMENGO'
+    elif new_name.upper().startswith(('BS2','BANCO BS2')):
+        new_name = 'BS2'
+    elif 'AGIBANK' in new_name.upper():
+        new_name = 'AGIBANK'
+    elif new_name.upper().startswith(('MEU PAG','MEUPAG')):
+        new_name = 'MEU PAG!'
+    elif new_name.upper().startswith('DIGIO'):
+        new_name = 'DIGIO'
+    elif new_name.upper().startswith(('PAN','BANCOPAN')):
+        new_name = 'PAN'
+    elif new_name.upper().startswith('CIELO'):
+        new_name = 'CIELO'
+    elif new_name.upper().startswith(('AME DIGITAL','AMEDIGITAL')):
+        new_name = 'AME DIGITAL'
+    elif new_name.upper().startswith('ITI'):
+        new_name = 'ITI'
+    elif new_name.upper().startswith('ITAUCARD'):
+        new_name = 'ITAUCARD'
+    elif new_name.upper().startswith('SANTANDER WAY'):
+        new_name = 'SANTANDER WAY'
+    elif new_name.upper().startswith('BRADESCO CART'):
+        new_name = 'BRADESCO CARTOES'
+    elif new_name.upper().startswith(('BANCO ITAÚ','ITAU')):
+        new_name = 'ITAU'
+    elif new_name.upper().startswith(('BRADESCO','BANCO.BRADESCO')):
+        new_name = 'BRADESCO'
+    elif new_name.upper().startswith('SANTANDER'):
+        new_name = 'SANTANDER'
+    elif new_name.upper().startswith(('BANCO DO BRASIL','BB')):
+        new_name = 'BANCO DO BRASIL'
+    elif new_name.upper().startswith('CAIXA'):
+        new_name = 'CAIXA ECONOMICA FEDERAL'
+    elif new_name.upper().startswith(('PAGBANK','PAGSEGURO')):
+        new_name = 'PAGSEGURO'
+
+    elif new_name == 'BB':
+        new_name = 'BANCO DO BRASIL'    
+    elif 'VIACREDI' in new_name:
+        new_name = 'VIACREDI'
+    elif 'SICOOB' in new_name:
+        new_name = 'SICOOB'
+    elif 'SICREDI' in new_name:
+        new_name = 'SICREDI'
+    elif 'UNIPRIME' in new_name:
+        new_name = 'UNIPRIME' 
+    elif 'UNICRED' in new_name:
+        new_name = 'UNICRED'
+    elif 'CRESOL' in new_name:
+        new_name = 'CRESOL'
+    elif 'ASCOOB' in new_name:
+        new_name = 'ASCOOB'
+    elif 'CREDJUST' in new_name:
+        new_name = 'CREDJUST'
+    elif new_name.startswith('NU '):
+        new_name = 'NUBANK'
+    elif new_name.startswith('COOPERATIVA') and new_name[-2] == '-':
+        new_name = new_name[-1]
+    else:
+        new_name = new_name.replace('INTERMEDIUM', 'INTER').replace('BANCO INTER','INTER')
+        new_name = new_name.replace('PAnew_nameRICANO', 'PAN').replace('BANCO PAN', 'PAN')
+        new_name = new_name.replace('BONSUCESSO', 'BS2').replace('BANCO BS2', 'BS2').replace('GRUPO BS2 BS2','BS2')
+        new_name = new_name.replace('BANCO NOSSA CAIXA', 'CAIXA ECONOMICA FEDERAL').replace('CAIXA ECONÔMICA FEDERAL', 'CAIXA ECONOMICA FEDERAL')
+        new_name = new_name.replace('SANTANDER BANESPA', 'SANTANDER')
+        new_name = new_name.replace('HSBC BANK BRASIL BANCO MULTIPLO', 'HSBC')
+        new_name = new_name.replace('BANCO DAYCOVAL','DAYCOVAL')
+        new_name = new_name.replace('BANCO BMG','BMG')
+        new_name = new_name.replace('BANCO CITIBANK','CITIBANK')
+        new_name = new_name.replace('BANCO ORIGINAL','ORIGINAL')
+        new_name = new_name.replace('PAGSEGURO INTERNET','PAGSEGURO')
+        new_name = new_name.replace('PAGSEGURO INTERNET','PAGSEGURO')
+        new_name = new_name.replace('BANCO BMC','BMC')
+        new_name = new_name.replace('ABCBRASIL','ABC-BRASIL')
+        new_name = new_name.replace('BANCO BGN','BGN')
+        new_name = new_name.replace('BANCO TOPÁZIO','BANCO TOPAZIO')
+        new_name = new_name.replace('NOVO BANCO CONTINENTAL BANCO MÚLTIPLO','NOVO BANCO CONTINENTAL BANCO MULTIPLO')
+        new_name = new_name.replace('AGIPLAN FINANCEIRA','AGIPLAN')
+        new_name = new_name.replace('BANIF INTERNACIONAL DO FUNCHAL (BRASIL) EM LIQUIDAÇÃO ORDINÁRIA','BANIF')
+        new_name = new_name.replace('BANCO DO ESTADO DO PARÁ','BANCO DO ESTADO DO PARA')
+        new_name = new_name.replace('BANCO DE TOKYO MITSUBISHI UFJ BRASIL','BANCO DE TOKYOMITSUBISHI UFJ BRASIL')
+        new_name = new_name.replace('BANCO A J RENNER','BANCO RENNER').replace('BANCO A.J. RENNER','BANCO RENNER')
+        new_name = new_name.replace('BANCO BM&FBOVESPA DE SERVIÇOS DE LIQUIDAÇÃO E CUSTÓDIA','BANCO BM&FBOVESPA').replace('BANCO BM FBOVESPA DE SERVICOS DE LIQUIDACAO E CUSTODIA','BANCO BM&FBOVESPA')
+        
+    if new_name.endswith(' S'):
+        new_name = new_name[:-2]
+    return new_name
---
+++ load_dados.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 15:18:31 2019
+
+@author: abonna
+"""
+
+import os
+import csv
+from collections import OrderedDict
+import credentials
+import psycopg2
+from subprocess import call
+from companies import parseName
+
+### Definicao das variaveis
+indir = '/home/ubuntu/dump/dados_consumidor_gov/'
+outdir = '/home/ubuntu/scripts/load-dados-consumidor-gov/parsed/'
+new_file = 'dados.csv'
+files = [f for f in os.listdir(indir) if f.endswith('.csv')]
+tablename = 'dados_consumidor_gov.dados_abertos_stg'
+
+DATABASE, HOST, USER, PASSWORD = credentials.setDatabaseLogin()
+
+### funcao que cria data no formato banco de dados
+def parse_date(date):
+    newdate = date[6:] + '-' + date[3:5] + '-' + date[:2]
+    return newdate
+
+### Iteracao sobre os diretorios e parser dos CSVs
+with open(outdir+new_file,'w', newline="\n", encoding="utf-8") as ofile: ### arquivo temporario
+    writer = csv.writer(ofile, delimiter=';')
+    for file in files:
+        with open(indir+file, 'r', encoding='latin-1') as ifile:
+            reader = csv.reader(ifile, delimiter=';')
+            for row in reader:
+                if row[8] in ['Varejo','Bancos, Financeiras e Administradoras de Cartão','Comércio Eletrônico','Empresas de Intermediação de Serviços / Negócios','Empresas de Pagamento Eletrônico']:
+                    row[5] = parse_date(row[5])
+                    row[7] = parseName(row[7])
+                    writer.writerow(row)
+
+#os.remove(outdir+file)
+
+### conecta no banco de dados
+db_conn = psycopg2.connect("dbname='{}' user='{}' host='{}' password='{}'".format(DATABASE, USER, HOST, PASSWORD))
+cursor = db_conn.cursor()
+print('Connected to the database')
+### copy
+with open(outdir+new_file, 'r') as ifile:
+    SQL_STATEMENT = "COPY %s FROM STDIN WITH CSV DELIMITER AS ';' NULL AS ''"
+    print("Executing Copy in "+tablename)
+    cursor.copy_expert(sql=SQL_STATEMENT % tablename, file=ifile)
+    db_conn.commit()
+cursor.close()
+db_conn.close()
+
+### VACUUM ANALYZE
+call('psql -d torkcapital -c "VACUUM VERBOSE ANALYZE '+tablename+'";',shell=True)
